a3f71f904b95a0cbacb4e4e192c1b52b87a8c8d5
garrettwilkin <garrett.wilkin@gmail.com>
2013-03-09 18:21:34 +0000

    more successful processing of Twilio response to record action

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var Telenode = require('telenode'),
     static = require('node-static'),
     fileServer = new static.Server('.'),
     url  = require('url'),
+    querystring  = require('querystring'),
     http = require('http');
 
 // Winston logging setup.
@@ -22,37 +23,63 @@ function smsHandler(err, body) {
   console.log('Body: ' + body);
 }
 
+function makeSms(twinfo) {
+  var message = 'From ' + twinfo.From + '\nStatus ' +
+		twinfo.CallStatus + '\nCity ' +
+		twinfo.CallerCity + '\nName ' +
+		twinfo.CallerName;
+  return message;
+}
+
+function sendAlertSms(twinfo) {
+  var message = makeSms(twinfo);
+  client.SMS.send(
+    {
+      from: creds.from,
+      to: creds.to,
+      body: message
+    },smsHandler
+  );
+  logger.info('SMS Message: '+message);
+}
+
 // This HTTP server listens for GET & POST requests from Twilio.
 logger.info('Starting TeleHacker');
 var port = 1340;
 var server = http.createServer(function(req,res) {
+  req.setEncoding('utf8');
+  req.content= '';
+
   var parsedUrl = url.parse(req.url,true);
   logger.info('HTTP '+req.method+' '+parsedUrl.pathname); 
+
   if  (parsedUrl.pathname === '/Incoming/Call') {
     if (req.method === 'GET') {
       fileServer.serveFile('./twiml/recordCall.xml', 200, {}, req, res);
       if (parsedUrl.query != '') {
         logger.info('Call details:\n'+util.inspect(parsedUrl.query));
-        var message = 'From ' + parsedUrl.query.From + '\nCallStatus ' +
-                      parsedUrl.query.CallStatus + '\nCallerCity ' +
-                      parsedUrl.query.CallerCity + '\nCallerName ' +
-                      parsedUrl.query.CallerName;
-        client.SMS.send(
-          {
-            from: creds.from,
-            to: creds.to,
-            body: message
-          },smsHandler
-        );
-        logger.info('SMS Message: '+message);
       }
+
     } else if (req.method === 'POST') {
       logger.info('Incoming call completed.');
+        logger.info('Call details:\n'+util.inspect(parsedUrl.query));
       res.statusCode = 200;
       res.end();
+      sendAlertSms(parsedUrl.query);
+
+      //Process Request Body.
+      req.addListener('data', function(chunk) {
+	req.content += chunk;
+      });
+      req.addListener('end', function(chunk) {
+	if (req.content != '') {
+	  var qs = querystring.parse(req.content);
+	  logger.info('Parsed Request data:\n'+util.inspect(qs));
+	}
+      });
     }
   } else {
+    logger.info('Non-supported path, serving static file');
     fileServer.serve(req,res);
   }
 }).listen(port);
-
